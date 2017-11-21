79872495730707aca508b53b8a773c9979fb00a8
dani199221 <danialnawaz@live.com>
2017-11-21 11:26:51 -0500

    adding libraries

--- python/controller.py
+++ python/controller.py
@@ -1,7 +1,7 @@
 import numpy as np
-
-
-e1 = np.array([1,0,0])
+import scipy.integrate as integrate
+import math
+from math import sin, cos, tan, expe1 = np.array([1,0,0])
 e2 = np.array([0,1,0])
 e3 = np.array([0,0,1])
 
