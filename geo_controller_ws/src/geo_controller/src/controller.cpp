01820ed2d128b2fd0137c067af8730da9f80f5b2
Malintha Fernando <malinthafer@gmail.com>
2017-11-22 16:41:34 -0500

    adding start-all.launch

--- geo_controller_ws/src/geo_controller/src/controller.cpp
+++ geo_controller_ws/src/geo_controller/src/controller.cpp
@@ -5,11 +5,42 @@
 #include <tf/transform_listener.h>
 #include <std_srvs/Empty.h>
 
+enum State
+{
+    Idle = 0,
+    Automatic = 1,
+    TakingOff = 2,
+    Landing = 3,
+};
+
+
+bool takeoff(
+        std_srvs::Empty::Request& req,
+        std_srvs::Empty::Response& res)
+{
+    ROS_INFO("Takeoff requested!");
+
+    tf::StampedTransform transform;
+//    m_listener.lookupTransform(m_worldFrame, m_frame, ros::Time(0), transform);
+//    m_startZ = transform.getOrigin().z();
+
+    return true;
+}
+
+bool land(
+        std_srvs::Empty::Request& req,
+        std_srvs::Empty::Response& res)
+{
+    ROS_INFO("Landing requested!");
+//    m_state = Landing;
+
+    return true;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "controller");
 
-    // Read parameters
     static ros::NodeHandle n("~");
     std::string worldFrame;
     n.param<std::string>("worldFrame", worldFrame, "/world");
@@ -18,14 +49,24 @@ int main(int argc, char **argv)
     double frequency;
     n.param("frequency", frequency, 50.0);
 
-//    Controller controller(worldFrame, frame, n);
-//    controller.run(frequency);
     ros::Rate rate = 30;
     int frame_count = 0;
+    float m_thrust = 40000;
+    ros::Publisher m_pubNav;
+    ros::NodeHandle nh;
+    ros::ServiceServer m_serviceTakeoff;
+    ros::ServiceServer m_serviceLand;
+
+    m_pubNav = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+    m_serviceTakeoff = nh.advertiseService("takeoff", &takeoff);
+    m_serviceLand = nh.advertiseService("land", &land);
 
     while(ros::ok()){
         rate.sleep();
-        ROS_INFO("Frame: %d", frame_count++);
+        ROS_INFO("Frame: %d bodyFrame: %s thrust: %f", frame_count++, frame.data(), m_thrust);
+        geometry_msgs::Twist msg;
+        msg.linear.z = m_thrust;
+        m_pubNav.publish(msg);
         ros::spinOnce();
     }
 
