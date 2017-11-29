fc760b3e2f9a4d49ad680185b19f361388aead36
Malintha Fernando <malinthafer@gmail.com>
2017-11-29 15:59:28 -0500

    impl for omega, x, x_dot from IMU and vicon

--- geo_controller_ws/src/geo_controller/src/GeoController.cpp
+++ geo_controller_ws/src/geo_controller/src/GeoController.cpp
@@ -6,8 +6,7 @@
 #include <std_srvs/Empty.h>
 #include "dynamicsImpl.h"
 
-enum State
-{
+enum State {
     Idle = 0,
     Automatic = 1,
     TakingOff = 2,
@@ -17,36 +16,30 @@ enum State
 class GeoController {
 
 public:
-    GeoController(const std::string& worldFrame, const std::string& frame,
-                  const ros::NodeHandle& n): m_serviceTakeoff(), m_serviceLand(), m_state(Idle), m_thrust(0),
-                                             m_startZ(0.0), m_worldFrame(worldFrame), m_bodyFrame(frame)
-
-    {
+    GeoController(const std::string &worldFrame, const std::string &frame,
+                  const ros::NodeHandle &n) : m_serviceTakeoff(), m_serviceLand(), m_state(Idle), m_thrust(0),
+                                              m_startZ(0.0), m_worldFrame(worldFrame), m_bodyFrame(frame) {
         ros::NodeHandle nh;
         m_listener.waitForTransform(m_worldFrame, m_bodyFrame, ros::Time(0), ros::Duration(10.0));
         m_pubNav = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
         m_serviceTakeoff = nh.advertiseService("takeoff", &GeoController::takeoff, this);
         m_serviceLand = nh.advertiseService("land", &GeoController::land, this);
-        dynamics = new dynamicsImpl(nh);
+        dynamics = new dynamicsImpl(nh, m_worldFrame, m_bodyFrame);
     };
 
 
-    void run(double frequency)
-    {
-        ros::NodeHandle node;
-        ros::Timer timer = node.createTimer(ros::Duration(1.0/frequency), &GeoController::iteration, this);
+    void run(double frequency) {
+        this->frequency = frequency;
+        ros::Timer timer = node.createTimer(ros::Duration(1.0 / frequency), &GeoController::iteration, this);
         ros::spin();
     }
 
 
-    void iteration(const ros::TimerEvent& e)
-    {
+    void iteration(const ros::TimerEvent &e) {
         float dt = e.current_real.toSec() - e.last_real.toSec();
 
-        switch(m_state)
-        {
-            case TakingOff:
-            {
+        switch (m_state) {
+            case TakingOff: {
                 tf::StampedTransform transform;
                 ROS_INFO("In taking off\n");
                 geometry_msgs::Twist msg;
@@ -70,8 +63,7 @@ public:
 
             }
                 break;
-            case Landing:
-            {
+            case Landing: {
                 m_goal.pose.position.z = m_startZ + 0.05;
                 tf::StampedTransform transform;
                 m_listener.lookupTransform(m_worldFrame, m_bodyFrame, ros::Time(0), transform);
@@ -82,8 +74,7 @@ public:
                 }
             }
 
-            case Automatic:
-            {
+            case Automatic: {
                 // get the current position of the drone
                 tf::StampedTransform transform;
                 m_listener.lookupTransform(m_worldFrame, m_bodyFrame, ros::Time(0), transform);
@@ -105,9 +96,18 @@ public:
 
             }
                 break;
-            case Idle:
-            {
+            case Idle: {
 //                ROS_INFO("In idle: Omega:%f \n", dynamics->temp_acc);
+                dynamics->setdt(dt);
+
+                Vector3d* x_arr = dynamics->get_x_v_Omega();
+                Vector3d x = x_arr[0];
+                Vector3d x_dot = x_arr[1];
+                Vector3d Omega = x_arr[2];
+
+                ROS_INFO("###tf dx dy dz: %f,%f,%f, dt: %f", Omega[0], Omega[1], Omega[2], dt);
+                ROS_INFO("\n");
+
                 geometry_msgs::Twist msg;
                 m_pubNav.publish(msg);
             }
@@ -127,10 +127,11 @@ private:
     float m_thrust;
     float m_startZ; // only for landing to the start z value
     geometry_msgs::PoseStamped m_goal;
-    dynamicsImpl* dynamics;
+    dynamicsImpl *dynamics;
+    float frequency;
+    ros::NodeHandle node;
 
-
-    bool takeoff(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res) {
+    bool takeoff(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res) {
         ROS_INFO("Takeoff requested!");
         tf::StampedTransform transform;
         m_state = TakingOff;
@@ -139,7 +140,7 @@ private:
         return true;
     }
 
-    bool land(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res) {
+    bool land(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res) {
         ROS_INFO("Landing requested!");
         m_state = Landing;
         return true;
@@ -148,20 +149,18 @@ private:
 };
 
 
-
-
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     ros::init(argc, argv, "controller");
 
     static ros::NodeHandle n("~");
     std::string worldFrame;
     n.param<std::string>("worldFrame", worldFrame, "/world");
     std::string frame;
-    n.getParam("frame", frame);
+    n.getParam("/crazyflie/geocontroller/frame", frame);
     double frequency;
-    n.param("frequency", frequency, 50.0);
+    n.param("frequency", frequency, 20.0);
+
+    ROS_INFO("### Initializing geocontroller. worldFrame:%s bodyFrame%s\n",worldFrame.data(), frame.data());
 
     GeoController controller(worldFrame, frame, n);
     controller.run(frequency);
--- geo_controller_ws/src/geo_controller/src/dynamicsImpl.h
+++ geo_controller_ws/src/geo_controller/src/dynamicsImpl.h
@@ -12,23 +12,37 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Imu.h"
 #include "geometry_msgs/Twist.h"
+#include <tf/transform_listener.h>
 
 using namespace Eigen;
 
-class dynamicsImpl{
+class dynamicsImpl {
 public:
     float temp_acc;
-    dynamicsImpl(ros::NodeHandle &n):nodeHandle(n) {
+
+    dynamicsImpl(ros::NodeHandle &n, std::string worldFrame, std::string bodyFrame) : nodeHandle(n),
+                                                                                      worldFrame(worldFrame),
+                                                                                      bodyFrame(bodyFrame) {
         n.getParam("/cf/crazyflie_add/tf_prefix", tf_prefix);
+
+        ROS_INFO("### Initialized dynamicsImpl worldFrame:%s bodyFrame%s\n",worldFrame.data(), bodyFrame.data());
+
+        transformListener.waitForTransform(worldFrame, bodyFrame, ros::Time(0), ros::Duration(10.0));
         m_subscribeImuMsgs = n.subscribe("/crazyflie/imu", 1, &dynamicsImpl::ImuValRecieved, this);
     }
 
-    void ImuValRecieved(const sensor_msgs::Imu::ConstPtr& msg) {
-
-        temp_acc = msg->angular_velocity.x;
-        Omega << msg->linear_acceleration.x,msg->linear_acceleration.y,msg->linear_acceleration.z;
+    void ImuValRecieved(const sensor_msgs::Imu::ConstPtr &msg) {
+        x_ddot << msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z;
+        Omega << msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z;
         //angular_velocity.y => forward (pitch), x => roll
+    }
 
+    /**
+     * set dt before executing any other function
+     * @param dt
+     */
+    void setdt(float dt) {
+        this->dt = dt;
     }
 
     Vector3d getOmega() {
@@ -36,12 +50,49 @@ public:
     }
 
 
+    Vector3d get_x_ddot() {
+        return x_ddot;
+    }
+
+    Vector3d* get_x_v_Omega() {
+        tf::StampedTransform transform;
+        transformListener.lookupTransform(worldFrame, bodyFrame, ros::Time(0), transform);
+
+        x << transform.getOrigin().x(), transform.getOrigin().y(), transform.getOrigin().z();
+        x_dot = (x - prev_x)/dt;
+
+
+        x_arr[0] = x;
+        x_arr[1] = x_dot;
+        x_arr[2] = Omega;
+
+        prev_x = x;
+        prev_x_dot = x_dot;
+        return x_arr;
+    }
+
+
+
 private:
     ros::NodeHandle &nodeHandle;
     ros::Subscriber m_subscribeImuMsgs;
+    tf::TransformListener transformListener;
+    ros::Publisher m_pubNav;
+
+    std::string worldFrame;
+    std::string bodyFrame;
+
+    float dt;
+
     std::string tf_prefix;
     Vector3d Omega;
+    Vector3d x_ddot;
     Vector3d x_dot;
+    Vector3d x;
+    Vector3d prev_x;
+    Vector3d prev_x_dot;
+
+    Vector3d x_arr[3];
 
 };
 
